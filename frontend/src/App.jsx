e013329eb9bf52f5a4c84c7b7bd1d4e78d86ce24
Karthik Mudaliar <mudaliarv41@gmail.com>
2026-07-30 20:15:43 +0530

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-
 import { RouterProvider } from 'react-router-dom'
 import { AppDataProvider } from './context/AppDataContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { router } from './router.jsx'
 import ErrorBoundary from './components/layout/ErrorBoundary.jsx'
-
 
 export default function App() {
   return (
